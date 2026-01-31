5cd3cd1f70a5138f52532c263fa337b8dc0a03ac
Tiago <up2023@up.pt>
2026-01-31 16:55:18 +0000

    need comments and to test it with the final circuit

--- src/buttons.c
+++ src/buttons.c
@@ -2,6 +2,16 @@
 #include <avr/io.h>
 #include <stddef.h>
 
+void select_button(struct button* button, struct button* buttons[]){
+    for (size_t i = 0; i < 6; i++)
+    {
+        if (PINC == (uint8_t)(1 << PINC0)){
+            *button = *buttons[i];
+            return;
+        }
+    }
+}
+
 void setup_buttons(struct button* buttons[]){
     /**
      * Predefined Button setup
@@ -12,12 +22,14 @@ void setup_buttons(struct button* buttons[]){
     static struct button button2 = {&PINC, PINC2, 0xFC03FB04}; // Volume DOWN
     static struct button button3 = {&PINC, PINC3, 0xFF00FB04}; // Channel UP
     static struct button button4 = {&PINC, PINC4, 0xFE01FB04}; // Channel DOWN
+    static struct button button5 = {&PINC, PINC4, 0};          // Special Button: Change 'message' of other buttons
 
     buttons[0] = &button0;
     buttons[1] = &button1;
     buttons[2] = &button2;
     buttons[3] = &button3;
     buttons[4] = &button4;
+    buttons[5] = &button5;
 }
 
 void change_message(struct button* button, uint32_t message){
--- src/main.c
+++ src/main.c
@@ -1,9 +1,47 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "leds.h"
+#include "buttons.h"
+#include "timers.h"
+#include <IRremote.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+void main(){
+    struct button buttons[6];
+    setup_ports();
+    setup_buttons();
 
-void loop() {
-  // put your main code here, to run repeatedly:
+    IrReceiver.begin(PINB0, ENABLE_LED_FEEDBACK);
+    IrSender.begin();
+
+    while (true)
+    {
+        _delay_ms(200);
+        struct button button;
+        while (!PINC);
+        select_button(&button, buttons);
+        switch (button.pin)
+        {
+        case PINC5:
+            _delay_ms(200);
+            struct button change_button;
+            blink(PORTB, PORTB2, ON);
+            while (true)
+            {
+                while (!PINC);
+                select_button(&change_button, buttons);
+                if (change_button.pin != button.pin) break;
+            }
+            while (!IrReceiver.decode());
+            blink(PORTB, PORTB2, OFF);
+            uint32_t message = IrReceiver.decodedIRData.decodedRawData;
+            change_message(&change_button, message);
+            IrReceiver.resume();
+            break;
+        
+        default:
+            send_message(button.message);
+            break;
+        }
+    }
 }
